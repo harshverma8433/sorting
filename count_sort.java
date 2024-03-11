2d39c1f263d72b21d5996da6b97d774ac759d135
harshverma8433 <harshverma8433@gmail.com>
2024-03-11 19:05:15 +0530

    sorting

--- count_sort.java
+++ count_sort.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 public class count_sort {
     public static void main(String[] args) {
         int[] arr = { 8, 4, 2, 4, 5, 8, 7, 9, 6, 8, 1, 2 };
-        int n = arr.length;
 
         int count[] = new int[10];
 
@@ -17,12 +16,11 @@ public class count_sort {
         for (int i = 1; i < count.length; i++) {
             count[i] = count[i] + count[i - 1];
         }
-        // 0 1 2 0 2 1 1 1 3 1
+        // 0 1 2 0 2 1 1 1 3  1
         // 0 1 3 3 5 6 7 8 11 12
 
         int[] result = new int[arr.length];
         int len = arr.length-1;
-        System.out.println(len );
         while (len >= 0) {
             count[arr[len]]--;
             result[count[arr[len]]] = arr[len];
--- radix_sort.java
+++ radix_sort.java
@@ -1,6 +1,49 @@
+import java.util.Arrays;
+
 public class radix_sort {
     public static void main(String[] args) {
         int[] arr = { 34, 12, 87, 456, 7 };
-        
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+
+        int l = 0;
+        for (int i = 1; i < max; i *= 10) {
+            count_sort(arr, i);
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void count_sort(int[] arr, int v) {
+        int[] result = new int[arr.length];
+        int[] count = new int[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            int lval = (arr[i] / v) % 10;
+            count[lval]++;
+        }
+
+        // update count array
+        for (int i = 1; i < count.length; i++) {
+            count[i] = count[i] + count[i - 1];
+        }
+
+        int l = arr.length - 1;
+        // 34, 12, 87, 456, 7
+        while (l >= 0) {
+            int val = (arr[l] / v) % 10;
+            count[val]--;
+            result[count[val]] = arr[l];
+            l--;
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            arr[i] = result[i];
+        }
+
     }
 }
